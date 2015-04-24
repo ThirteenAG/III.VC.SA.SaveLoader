974040481e7b5a82ecbeb3609ac9b1a3b7ec9680
ThirteenAG <thirteenag@gmail.com>
2015-04-24 14:52:43 +0300

    SaveLoader v1.1

--- SaveLoader_gta3vcsa/dllmain.cpp
+++ SaveLoader_gta3vcsa/dllmain.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "IniReader.h"
 #include "injector\injector.hpp"
+#include "injector\hooking.hpp"
 #include <string>
 //#include <WinInet.h>
 //#pragma comment(lib,"wininet.lib")
@@ -14,25 +15,127 @@ int *funcGameVersion;
 int *loadingStage;
 int *userdirPath;
 char *isInMenu;
-int *_saveFileName;
+int *saveFileName;
 char(__thiscall *LoadSave)(int, char);
 int to_int(char const *s);
 char* szCustomUserFilesDirectoryInGameDir;
 
+void FindFiles()
+{
+	if (!saveNum)
+	{
+		char SFPath[MAX_PATH];
+		strcpy(SFPath, (char *)userdirPath);
+		strcat(SFPath, "*.b");
+
+		WIN32_FIND_DATA fd;
+		HANDLE File = FindFirstFile(SFPath, &fd);
+
+		FILETIME LastWriteTime = fd.ftLastWriteTime;
+
+		if (File != INVALID_HANDLE_VALUE)
+		{
+			do
+			{
+				if (CompareFileTime(&fd.ftLastWriteTime, &LastWriteTime) >= 0)
+				{
+					if (GetFileSize(File, NULL) >= 200820)
+					{
+						//MessageBox(0, 0, fd.cFileName, 0);
+						LastWriteTime = fd.ftLastWriteTime;
+						saveNum = to_int(fd.cFileName + 4);
+					}
+				}
+
+			} while (FindNextFile(File, &fd));
+			FindClose(File);
+		}
+	}
+}
+
+template<uintptr_t addr>
+void FrontendIdleHook()
+{
+	using printstr_hook = injector::function_hooker<addr, void()>;
+	injector::make_static_hook<printstr_hook>([](printstr_hook::func_type FrontendIdle)
+	{
+		static int nTimes = 0;
+
+		FindFiles();
+
+		// On the second processing tick, do the game start
+		if (++nTimes == 2)
+		{
+			bool bNoLoad = (GetAsyncKeyState(VK_SHIFT) & 0xF000) != 0;
+			if (bNoLoad)
+			{
+				return FrontendIdle();
+			}
+
+			auto& gvm = address_manager::singleton();
+
+			if (gvm.IsSA())
+			{
+				if (saveNum && saveNum != 129)
+				{
+					WriteMemory<char>(LastUsedSlot, saveNum - 1);
+					LoadSave(CMenuManagerStruct, currentMenuItem);
+				}
+				else
+				{
+					WriteMemory<char>(CMenuManagerStruct + 0x5C, 0);
+					WriteMemory(StartNewGame, 0);
+				}
+			}
+			else
+			{
+				if (saveNum && saveNum != 129)
+				{
+					WriteMemory<char>(LastUsedSlot, saveNum - 1);
+					WriteMemory<char>(LoadSave_3vc, currentMenuItem);
+				}
+				else
+				{
+					WriteMemory(LoadSave_3vc, StartNewGame);
+
+					if (gvm.IsIII())
+					{
+						WriteMemory<char>(CMenuManagerStruct + (0x8F5AED - 0x8F59D8), 1);
+						WriteMemory<char>(CMenuManagerStruct + (0x8F5AE9 - 0x8F59D8), 0);
+						WriteMemory<char>(CMenuManagerStruct + (0x8F5AEC - 0x8F59D8), 1);
+						WriteMemory<char>(CMenuManagerStruct + (0x8F5E2C - 0x8F59D8), 1);
+					}
+					else if (gvm.IsVC())
+					{
+						WriteMemory<char>(CMenuManagerStruct + (0x869641 - 0x869630), 1);
+						WriteMemory<char>(CMenuManagerStruct + (0x869668 - 0x869630), 0);
+						WriteMemory<char>(CMenuManagerStruct + (0x869669 - 0x869630), 1);
+						WriteMemory<char>(CMenuManagerStruct + (0x86966C - 0x869630), 1);
+					}
+				}
+			}
+		}
+
+		return FrontendIdle();
+	});
+}
+
 char* __cdecl InitUserDirectories()
 {
+	CreateDirectory(szCustomUserFilesDirectoryInGameDir, NULL);
 	return szCustomUserFilesDirectoryInGameDir;
 }
 
 DWORD WINAPI Init(LPVOID param)
 {
-
 	CIniReader iniReader("");
 	if ((!iniReader.ReadInteger("MAIN", "Enable", 1))) {
 		return 0;
 	}
 
+	saveNum = iniReader.ReadInteger("MAIN", "LoadSlot", 0);
 	bool bSkipIntro = iniReader.ReadInteger("MAIN", "SkipIntro", 1) == 1;
+	bool bDisableLoadingScreens = iniReader.ReadInteger("MAIN", "DisableLoadingScreens", 1) == 1;
 
 	szCustomUserFilesDirectoryInGameDir = iniReader.ReadString("MAIN", "CustomUserFilesDirectoryInGameDir", "");
 
@@ -45,6 +148,11 @@ DWORD WINAPI Init(LPVOID param)
 			if (bSkipIntro)
 			{
 				injector::WriteMemory<char>(0x582A7B, 0x05, true);
+			}
+
+			if (bDisableLoadingScreens)
+			{
+				injector::WriteMemory<uint8_t>(0x48D770, 0xC3, true);
 			}
 
 			if (strncmp(szCustomUserFilesDirectoryInGameDir, "0", 1) != 0)
@@ -61,36 +169,26 @@ DWORD WINAPI Init(LPVOID param)
 			isInMenu = (char *)CMenuManagerStruct + 0x457;
 			LastUsedSlot = CMenuManagerStruct + 0x55C;
 			LoadSave_3vc = CMenuManagerStruct + 0x548;
-			_saveFileName = (int *)0x8E2CBC;
+			saveFileName = (int *)0x8E2CBC;
 			StartNewGame = 10;
+			WriteMemory<char>(0x485134, 0x10, true);
+			FrontendIdleHook<(0x48E90F)>();
 		}
 		else
 		{
-			if (gvm.GetMajorVersion() == 1 && gvm.GetMinorVersion() == 1)
+			if (gvm.IsSteam())
 			{
-				loadingStage = (int *)0x8F58EC;
-				userdirPath = (int *)0x8E2870;
-				CMenuManagerStruct = 0x8F5A8C;
+				loadingStage = (int *)0x905A2C;
+				userdirPath = (int *)0x8F29B0;
+				currentMenuItem = 14;
+				CMenuManagerStruct = 0x905BCC;
 				isInMenu = (char *)CMenuManagerStruct + 0x457;
 				LastUsedSlot = CMenuManagerStruct + 0x55C;
 				LoadSave_3vc = CMenuManagerStruct + 0x548;
-				_saveFileName = (int *)0x8E2D70;
+				saveFileName = (int *)0x8F2EB0;
 				StartNewGame = 10;
-			}
-			else
-			{
-				if (gvm.IsSteam())
-				{
-					loadingStage = (int *)0x905A2C;
-					userdirPath = (int *)0x8F29B0;
-					currentMenuItem = 14;
-					CMenuManagerStruct = 0x905BCC;
-					isInMenu = (char *)CMenuManagerStruct + 0x457;
-					LastUsedSlot = CMenuManagerStruct + 0x55C;
-					LoadSave_3vc = CMenuManagerStruct + 0x548;
-					_saveFileName = (int *)0x8F2EB0;
-					StartNewGame = 10;
-				}
+				WriteMemory<char>(0x485204, 0x10, true);
+				FrontendIdleHook<(0x48E95F)>();//
 			}
 		}
 	}
@@ -108,6 +206,12 @@ DWORD WINAPI Init(LPVOID param)
 					injector::MakeJMP(0x601A45, 0x601B2F, true);
 				}
 
+				if (bDisableLoadingScreens)
+				{
+					injector::WriteMemory<uint8_t>(0x4A69D0, 0xC3, true);
+					injector::MakeJMP(0x495809, 0x49596E, true); //outro
+				}
+
 				if (bSkipIntro)
 				{
 					injector::WriteMemory<char>(0x5FFFAB, 0x05, true);
@@ -120,37 +224,24 @@ DWORD WINAPI Init(LPVOID param)
 				isInMenu = (char *)CMenuManagerStruct + 0x38;
 				LastUsedSlot = CMenuManagerStruct + 0x100;
 				LoadSave_3vc = CMenuManagerStruct + 0xF8;
-				_saveFileName = (int *)0x978428;
+				saveFileName = (int *)0x978428;
 				StartNewGame = 7;
+				FrontendIdleHook<(0x4A5BF2)>();
 			}
 			else
 			{
-				if (gvm.GetMajorVersion() == 1 && gvm.GetMinorVersion() == 1)
+				if (gvm.IsSteam())
 				{
-					loadingStage = (int *)0x9B5F10;
-					userdirPath = (int *)0x9750A4;
+					loadingStage = (int *)0x9B4F10;
+					userdirPath = (int *)0x9740A4;
 					currentMenuItem = 12;
-					CMenuManagerStruct = 0x869638;
+					CMenuManagerStruct = 0x868638;
 					isInMenu = (char *)CMenuManagerStruct + 0x38;
 					LastUsedSlot = CMenuManagerStruct + 0x100;
 					LoadSave_3vc = CMenuManagerStruct + 0xF8;
-					_saveFileName = (int *)0x978430;
+					saveFileName = (int *)0x977430;
 					StartNewGame = 7;
-				}
-				else
-				{
-					if (gvm.IsSteam())
-					{
-						loadingStage = (int *)0x9B4F10;
-						userdirPath = (int *)0x9740A4;
-						currentMenuItem = 12;
-						CMenuManagerStruct = 0x868638;
-						isInMenu = (char *)CMenuManagerStruct + 0x38;
-						LastUsedSlot = CMenuManagerStruct + 0x100;
-						LoadSave_3vc = CMenuManagerStruct + 0xF8;
-						_saveFileName = (int *)0x977430;
-						StartNewGame = 7;
-					}
+					FrontendIdleHook<(0x4A5AC2)>();
 				}
 			}
 		}
@@ -174,107 +265,13 @@ DWORD WINAPI Init(LPVOID param)
 					CMenuManagerStruct = 0xBA6748;
 					isInMenu = (char *)CMenuManagerStruct + 0x5C;
 					LastUsedSlot = CMenuManagerStruct + 0x15F;
-					_saveFileName = (int *)0xC16DB8;
+					saveFileName = (int *)0xC16DB8;
 					StartNewGame = 0xB7CB49;
+					FrontendIdleHook<(0x53ECCB)>();
 				}
 			}
 		}
 	}
-
-	while (*loadingStage < 7 || !*isInMenu)
-	{
-		Sleep(20);
-		if (GetAsyncKeyState(VK_SHIFT))
-		{
-			return 0;
-		}
-	}
-	
-	TCHAR SFPath[MAX_PATH];
-	std::strcpy(SFPath, (char *)userdirPath);
-	std::strcat(SFPath, "*.b");
-
-	saveNum = iniReader.ReadInteger("MAIN", "LoadSlot", 0);
-
-	if (!saveNum)
-	{
-		WIN32_FIND_DATA info;
-		HANDLE file = FindFirstFile(SFPath, &info);
-
-		if (file != INVALID_HANDLE_VALUE) {
-			struct FileInfo {
-				HANDLE h;
-				WIN32_FIND_DATA info;
-			} newest;
-
-			newest.h = file;
-			newest.info = info;
-
-			while (FindNextFile(file, &info)) {
-				if (CompareFileTime(&info.ftLastWriteTime, &newest.info.ftLastWriteTime) > 0) {
-					newest.h = file;
-					newest.info = info;
-				}
-			}
-
-			DWORD dwFileSize = GetFileSize(newest.h, NULL);
-			if (dwFileSize >= (int)200820)
-			{
-				std::strcpy(SFPath, newest.info.cFileName);
-				saveNum = to_int(SFPath + 4);
-			}
-			FindClose(file);
-		}
-	}
-
-	if (gvm.IsSA())
-	{
-		do
-		{
-			if (saveNum && saveNum != 129)
-			{
-				Sleep(100); //fastloader.asi compatibility
-				WriteMemory(LastUsedSlot, saveNum - 1);
-				LoadSave(CMenuManagerStruct, currentMenuItem);
-			}
-			else
-			{
-				Sleep(100);
-				WriteMemory(CMenuManagerStruct + 0x5C, 0);
-				WriteMemory(StartNewGame, 0);
-			}
-		} while (*loadingStage < 8);
-	} 
-	else
-	{
-		do
-		{
-			if (saveNum && saveNum != 129)
-			{
-				WriteMemory(LoadSave_3vc, currentMenuItem);
-			}
-			else
-			{
-				WriteMemory(LoadSave_3vc, StartNewGame);
-
-				if (gvm.IsIII())
-				{
-					WriteMemory(CMenuManagerStruct + (0x8F5AED - 0x8F59D8), 1);
-					WriteMemory(CMenuManagerStruct + (0x8F5AE9 - 0x8F59D8), 0);
-					WriteMemory(CMenuManagerStruct + (0x8F5AEC - 0x8F59D8), 1);
-					WriteMemory(CMenuManagerStruct + (0x8F5E2C - 0x8F59D8), 1);
-				}
-				else if (gvm.IsVC())
-				{
-					WriteMemory(CMenuManagerStruct + (0x869641 - 0x869630), 1);
-					WriteMemory(CMenuManagerStruct + (0x869668 - 0x869630), 0);
-					WriteMemory(CMenuManagerStruct + (0x869669 - 0x869630), 1);
-					WriteMemory(CMenuManagerStruct + (0x86966C - 0x869630), 1);
-				}
-			}
-		} while (*loadingStage < 8);
-	}
-
 	return 0;
 }
 
